e7d2b1365d96cb46241c77a627a2f70f78f1dc3b
Vladislav Bakushkin <bakushkinv@yandex.ru>
2024-09-07 11:44:50 +0300

    init project

---
+++ src/main/java/org/bakushkin/springuserprofile/SpringUserProfileApplication.java
@@ -0,0 +1,13 @@
+package org.bakushkin.springuserprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUserProfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUserProfileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/bakushkin/springuserprofile/SpringUserProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bakushkin.springuserprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUserProfileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
